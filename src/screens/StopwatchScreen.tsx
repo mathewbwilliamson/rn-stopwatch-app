32091a42fa3e527c0410e4573268855edc1b033f
Matt Williamson <mathewbwilliamson@gmail.com>
2021-01-12 23:08:13 -0500

    Added ability to add multiple stopwatches

--- src/screens/StopwatchScreen.tsx
+++ src/screens/StopwatchScreen.tsx
@@ -1,13 +1,47 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { NavigationStackScreenComponent } from 'react-navigation-stack';
 import { Stopwatch } from '../components/Stopwatch';
 
+interface Stopwatch {
+    id: number;
+}
+
 export const StopwatchScreen: NavigationStackScreenComponent = () => {
+    const [stopwatchCount, setStopwatchCount] = React.useState<Stopwatch[]>([
+        {
+            id: 0,
+        },
+    ]);
+
+    const removeStopwatch = (stopwatchId: number) => {
+        setStopwatchCount(
+            stopwatchCount.filter((item) => item.id !== stopwatchId)
+        );
+    };
+
+    console.log('\x1b[41m%s \x1b[0m', '[matt] stopwatchCount', stopwatchCount);
     return (
         <SafeAreaView>
-            <Stopwatch />
+            {Object.keys(stopwatchCount).map((item, idx) => {
+                const thing = stopwatchCount[idx];
+
+                return <Stopwatch key={thing.id} />;
+            })}
+            <Button
+                title="Add Stopwatch"
+                onPress={() =>
+                    setStopwatchCount([
+                        ...stopwatchCount,
+                        {
+                            id:
+                                stopwatchCount[stopwatchCount.length - 1].id +
+                                1,
+                        },
+                    ])
+                }
+            />
         </SafeAreaView>
     );
 };
