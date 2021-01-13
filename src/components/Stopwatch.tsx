0769e31e9a467940cca0a95ed17078ba4d434fab
Matt Williamson <mathewbwilliamson@gmail.com>
2021-01-12 23:34:32 -0500

    Slight refactor for stopwatch utils

--- src/components/Stopwatch.tsx
+++ src/components/Stopwatch.tsx
@@ -1,17 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
-
-export const formatTime = (rawTime: number) => {
-    const getSeconds = `0${rawTime % 60}`.slice(-2);
-
-    const minutes = `${Math.floor(rawTime / 60)}`;
-    const getMinutes = `0${Number(minutes) % 60}`.slice(-2);
-
-    const getHours = `0${Math.floor(rawTime / 3600)}`.slice(-2);
-
-    return `${getHours}:${getMinutes}:${getSeconds}`;
-};
+import { formatTime } from './utils/stopwatchUtils';
 
 interface StopwatchProps {
     onRemoveStopwatch: (stopwatchId: number) => void;
